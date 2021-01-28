dc6c677e10cb8039bc2fb41cc3b60bb26c8b5ff2
Greatness7 <Greatness7@gmail.com>
2021-01-28 03:34:19 -0500

    do multiple steps of raycasting, and add debug visualization helpers

--- MWSE/mods/mantle/main.lua
+++ MWSE/mods/mantle/main.lua
@@ -13,35 +13,95 @@ local isClimbing = false
 local UP = tes3vector3.new(0, 0, 1)
 local DOWN = tes3vector3.new(0, 0, -1)
 
--- get the ray start pos, from above and slightly front downwards
-local function frontDownCast()
-    local eyeVec = tes3.getPlayerEyeVector()
-    local eyePos = tes3.getPlayerEyePosition()
 
-    -- renormalize eyevec with fixed magnitude for zero z
-    -- to avoid making a spherical reach
-    local vm = math.sqrt(eyeVec.x * eyeVec.x + eyeVec.y * eyeVec.y)
-    if vm <= 0 then
-        vm = 1
+--
+local function debugPlaceWidget(widgetId, position, intersection)
+    local root = tes3.game.worldSceneGraphRoot.children[9]
+    assert(root.name == "WorldVFXRoot")
+
+    local node = root:getObjectByName(widgetId)
+    if not node then
+        node = tes3.loadMesh("g7\\widget_raytest.nif"):clone()
+        node.name = widgetId
+        root:attachChild(node)
+    end
+    node.translation = intersection
+    node:update()
+
+    local base = node:getObjectByName("Base")
+    local t = base.parent.worldTransform
+    base.translation = (t.rotation * t.scale):invert() * (position - t.translation)
+    base:update()
+
+    root:update()
+    root:updateProperties()
+    root:updateNodeEffects()
+end
+
+-- alternative rayTest function that also places a visualization
+local function debugRayTest(t)
+    local rayhit = tes3.rayTest(t)
+    if t.widgetId and rayhit then
+        debugPlaceWidget(t.widgetId, t.position, rayhit.intersection)
+    end
+    return rayhit
+end
+---
+
+
+local function getClimbingDestination()
+    local position = tes3.player.position:copy()
+    local direction = tes3.getPlayerEyeVector()
+
+    -- clear direction z component and re-normalize
+    -- this creates a "forward" vector without tilt
+    direction.z = 0
+    direction:normalize()
+
+    -- get the minimum obstacle height (pc waist)
+    local minHeight = position.z + tes3.mobilePlayer.height * 0.5
+
+    -- we do raycasts from 200 units above player
+    position.z = position.z + 200
+
+    -- variable for holding the final destination
+    local destination = { z = -math.huge }
+
+    -- doing 3 raycasts of varying amounts forward
+    for i, unitsForward in ipairs{99, 66, 33} do
+        local rayhit = debugRayTest{
+            widgetId = ("widget_%s"):format(i),
+            position = position + (direction * unitsForward),
+            direction = DOWN,
+            ignore = {tes3.player},
+        }
+        if rayhit then
+            -- only keep the intersection with highest z
+            -- and only if it is higher than the minimum
+            if (rayhit.intersection.z > destination.z
+                and rayhit.intersection.z > minHeight)
+            then
+                destination = rayhit.intersection:copy()
+            end
+        end
     end
 
-    local pos = tes3vector3.new(
-        eyePos.x + (eyeVec.x / vm * 75),
-        eyePos.y + (eyeVec.y / vm * 75),
-        eyePos.z + (200)
-    )
-
-    return tes3.rayTest{position = pos, direction = DOWN}
+    -- if x/y are undefined then all racasts failed
+    if destination.x and destination.y then
+        return destination
+    end
 end
+
 
 local function getCeilingDistance()
     local eyePos = tes3.getPlayerEyePosition()
-    local result = tes3.rayTest{position = eyePos, direction = UP}
+    local result = tes3.rayTest{position = eyePos, direction = UP, ignore={tes3.player}}
     if result then
         return result.distance
     end
     return math.huge
 end
+
 
 local function applyClimbingFatigueCost(mob)
     local jumpBase = tes3.findGMST('fFatigueJumpBase').value
@@ -51,30 +111,31 @@ local function applyClimbingFatigueCost(mob)
     mob.fatigue.current = math.max(0, mob.fatigue.current - fatigueCost)
 end
 
+
 local function climbPlayer(currentZ, destinationZ, speed)
     -- some bias to prevent clipping through floors
-    if getCeilingDistance() < 20 then
-        return
+    if getCeilingDistance() >= 20 then
+        -- equalizing instead gets consistent results
+        tes3.player.position.z = currentZ + (destinationZ / 60 * speed * fatigueInfluence)
+
+        -- tiny amount of velocity cancellation
+        -- not zero, it disables gravity impact
+        tes3.mobilePlayer.velocity = tes3vector3.new(0.01, 0.01, 0.01)
+        tes3.mobilePlayer.impulseVelocity = tes3vector3.new(0.01, 0.01, 0.01)
     end
-
-    -- equalizing instead gets consistent results
-    tes3.player.position.z = currentZ + (destinationZ / 60 * speed * fatigueInfluence)
-
-    -- tiny amount of velocity cancellation
-    -- not zero, it disables gravity impact
-    tes3.mobilePlayer.velocity = tes3vector3.new(0.01, 0.01, 0.01)
-    tes3.mobilePlayer.impulseVelocity = tes3vector3.new(0.01, 0.01, 0.01)
-
     return tes3.player.position.z
 end
+
 
 local function playClimbingStartedSound()
     tes3.playSound{sound = 'corpDRAG', volume = 0.4, pitch = 0.8}
 end
 
+
 local function playClimbingFinishedSound()
     tes3.playSound{sound = 'corpDRAG', volume = 0.1, pitch = 1.3}
 end
+
 
 local function startClimbing(destination, speed)
     applyClimbingFatigueCost(tes3.mobilePlayer)
@@ -99,6 +160,7 @@ local function startClimbing(destination, speed)
     --mobilePlayer:exerciseSkill(tes3.skill.acrobatics, 1)
 end
 
+
 local function onClimbE(e)
     local playerMob = tes3.mobilePlayer
 
@@ -106,8 +168,8 @@ local function onClimbE(e)
         return
     elseif tes3ui.menuMode() then
         return
-    elseif tes3.is3rdPerson() then
-        return
+    -- elseif tes3.is3rdPerson() then
+    --     return
     elseif playerMob.isFlying then
         return
     end
@@ -123,26 +185,14 @@ local function onClimbE(e)
         return
     end
 
-    -- down raycast
-    local destRayHit = frontDownCast()
-    if (destRayHit == nil) then
-        return
-    end
-
-    -- bail if already higher than destination
-    local zPos = playerMob.position.z
-    if zPos >= destRayHit.intersection.z then
-        return
-    end
-
     -- if there is enough room for PC height go on
     if getCeilingDistance() < playerMob.height then
         return
     end
 
-    -- if below waist obstacle, do not attempt climbing
-    local waistHeight = playerMob.height * 0.5
-    if destRayHit.intersection.z < (zPos + waistHeight) then
+    -- down raycast
+    local destination = getClimbingDestination()
+    if (destination == nil) then
         return
     end
 
@@ -167,7 +217,7 @@ local function onClimbE(e)
 
     -- how much to move upwards
     -- bias for player bounding box
-    local destination = (destRayHit.intersection.z - zPos) * acrobaticsInfluence + 70
+    local destination = (destination.z - playerMob.position.z) * acrobaticsInfluence + 70
     startClimbing(destination, speed)
 end
 event.register('keyDown', onClimbE, {filter = tes3.scanCode.e})
